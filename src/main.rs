bc5d482458014b8ae87b8971f216b62437b8e922
A3Tom <Tom_Esq@Yahoo.com>
2023-03-12 22:41:08 +0000

    Fleshed out main and added user input & comparison on guess vs rand output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{io, cmp::Ordering};
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number between 1 & 100");
+
+    let correct_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The right number is {:#} btw", correct_number);
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Canny read what ye wrote mate");
+
+    let guess: u32 = guess.trim()
+        .parse()
+        .expect("Haul, geez a number!");
+
+    println!("You guessed: {:#}", guess);
+
+    match guess.cmp(&correct_number) {
+        Ordering::Less => println!("Too low"),
+        Ordering::Equal => println!("Fuck yeah you win, well played you nutter... how long did this take you?"),
+        Ordering::Greater => todo!("Too high"),
+    }
 }
